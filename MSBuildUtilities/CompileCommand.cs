26ba25c253f6d89ee10f509bc9c8b6a7f2fbf785
Andris Nyiscsák <a.nyiscsak@gaijin.team>
2025-02-01 16:38:10 +0100

    Using the jam's compile_commands.json generation to get the compile arguments of the selected files

---
+++ MSBuildUtilities/CompileCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Gaijin.MSBuild.Utilities;
+
+internal sealed class CompileCommand
+{
+    [JsonProperty("directory")]
+    public string Directory { get; set; }
+
+    [JsonProperty("file")]
+    public string File { get; set; }
+
+    [JsonProperty("arguments")]
+    public List<string> Arguments { get; set; }
+
+    [JsonProperty("output")]
+    public string Output { get; set; }
+}
--- MSBuildUtilities/GetCompileCmdOfSelectedFiles.cs
+++ MSBuildUtilities/GetCompileCmdOfSelectedFiles.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
 
 namespace Gaijin.MSBuild.Utilities;
 
@@ -26,84 +26,52 @@ public sealed class GetCompileCmdOfSelectedFiles : Microsoft.Build.Utilities.Tas
         List<FileDesc> selectedFiles = [];
         for (int i = 0; i < SelectedFiles.Length; i++)
         {
-            selectedFiles.Add(new FileDesc(SelectedFiles[i].Replace('\\', '/'), i));
+            selectedFiles.Add(new(SelectedFiles[i].Replace('\\', '/'), i));
         }
 
         List<TaskItem> commands = [];
 
-        void runJam(Process jamBuild)
+        using (Process jamBuild = new()
         {
-            jamBuild.StartInfo.UseShellExecute = false;
-            jamBuild.StartInfo.RedirectStandardOutput = true;
-            jamBuild.StartInfo.FileName = "jam";
-            jamBuild.StartInfo.WorkingDirectory = WorkDirectory;
-
-            jamBuild.OutputDataReceived += (s, e) =>
+            StartInfo = new()
             {
-                bool filtered = false;
-                var line = e.Data.AsSpan();
-                if (line.StartsWith("  call_filtered ".AsSpan()))
-                {
-                    line = line.Slice(16, line.Length - 19);
-                    filtered = true;
-                }
-                else if (line.StartsWith("  call ".AsSpan()))
-                {
-                    line = line.Slice(7);
-                }
-                else
-                {
-                    return;
-                }
+                Arguments = BuildCommand + " -sDumpBuildCmd=yes -d0 -a -n",
+                FileName = "jam",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                WorkingDirectory = WorkDirectory,
+            }
+        })
+        {
+            var serializer = JsonSerializer.Create();
 
+            jamBuild.Start();
+
+            using JsonTextReader jsonReader = new(jamBuild.StandardOutput);
+
+            jsonReader.Read(); // move to start of first object
+            while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndArray)
+            {
+                var compileCommand = serializer.Deserialize<CompileCommand>(jsonReader);
                 for (int i = 0; i < selectedFiles.Count; i++)
                 {
-                    if (line.EndsWith(selectedFiles[i].File.AsSpan()))
+                    if (compileCommand.File.EndsWith(selectedFiles[i].File))
                     {
-                        Span<char> command = stackalloc char[line.Length];
-                        line.CopyTo(command);
-
-                        for (int j = 0; j < ExcludeArgs.Length; j++)
-                            for (int index = command.IndexOf(ExcludeArgs[j].AsSpan()), k = index; k < index + ExcludeArgs[j].Length; k++)
-                                command[k] = ' ';
-
-                        if (filtered)
-                        {
-                            int index = command.IndexOf("#\\(".AsSpan());
-                            command[index] = ' ';
-                            command[index + 1] = ' ';
-                            command[index + 2] = ' ';
-                        }
-
-                        TaskItem commandItem = new(command.ToString());
+                        TaskItem commandItem = new(string.Join(" ", compileCommand.Arguments));
                         commandItem.SetMetadata("File", SelectedFiles[selectedFiles[i].Index]);
                         commands.Add(commandItem);
                         selectedFiles.RemoveAt(i);
 
                         if (selectedFiles.Count == 0)
-                            jamBuild.Kill();
+                            goto KillJamBuild;
 
                         break;
                     }
                 }
-            };
+            }
 
-            jamBuild.Start();
-            jamBuild.BeginOutputReadLine();
-            jamBuild.WaitForExit();
-        }
-
-        {
-            using Process jamBuild = new();
-            jamBuild.StartInfo.Arguments = BuildCommand + " -sSkipDigestProecssing -n -dx";
-            runJam(jamBuild);
-        }
-
-        if (selectedFiles.Count > 0)
-        {
-            using Process jamBuild = new();
-            jamBuild.StartInfo.Arguments = BuildCommand + " -sSkipDigestProecssing -n -dx -a";
-            runJam(jamBuild);
+        KillJamBuild:
+            jamBuild.Kill();
         }
 
         Commands = [.. commands];
